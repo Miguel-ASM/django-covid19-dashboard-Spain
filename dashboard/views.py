6330feb35005014a0f862c9385ad3aace6d94c9d
miguel <19872243+Miguel-ASM@users.noreply.github.com>
2020-05-12 11:23:30 +0200

    created index view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import plotlytest
 
+def index(request):
+    example_plot = plotlytest.plot_somethin()
+    return render(request,'dashboard/index.html',{'example_plot':example_plot})
 # Create your views here.
